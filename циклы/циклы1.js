d027a31f00b0afc057d052aaafe76b1a95a581a4
Galkin Alexey Andreevich <97546730+GalkinAlexeyAndreevich@users.noreply.github.com>
2022-02-03 20:27:44 +0300

    Исправление циклы.js

---
+++ циклы/циклы1.js
@@ -0,0 +1,18 @@
+let mass = []
+let n = 40
+let m = -5
+let count = 42
+let i = 0
+// while(mass.length < count){
+//     num = Math.round(Math.random()*Math.abs(m-n) + Math.min(n,m))
+//     mass.push(num)
+// }
+// do{
+//     num = Math.round(Math.random()*Math.abs(m-n) + Math.min(n,m))
+//     mass.push(num)
+// }while(mass.length < count)
+// for(i=0; i < count;i++){
+//     num = Math.round(Math.random()*Math.abs(m-n) + Math.min(n,m))
+//     mass.push(num)  
+// }
+console.log(mass)
