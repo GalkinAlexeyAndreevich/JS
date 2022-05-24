c9d9ce153db9dd6f42b69faa0e96180c946d17a4
Galkin Alexey Andreevich <97546730+GalkinAlexeyAndreevich@users.noreply.github.com>
2022-05-24 17:46:07 +0300

    Add files via upload

--- Js учебная практика/main.js
+++ Js учебная практика/main.js
@@ -1,8 +1,27 @@
-const timerItems = document.querySelectorAll(".timer__count")
-console.log(timerItems)
+
+
 // getHours(), getMinutes(), getSeconds()
-const today = new Date()
-let days = today.getDate() 
-let hours = today.getHours()
-let minutes = today.getMinutes()
-let seconds = today.getSeconds()
+
+function getToday(){
+    const timerItems = document.querySelectorAll(".timer__count")
+    console.log(timerItems)
+    const today = new Date()
+    let arrToday = [today.getDate(),today.getHours(),today.getMinutes(),today.getSeconds()]
+    // let days = today.getDate() 
+    // let hours = today.getHours()
+    // let minutes = today.getMinutes()
+    // let seconds = today.getSeconds() 
+    for(let i=0; i<arrToday.length; i++){
+        timerItems[i].textContent =  arrToday[i]
+    }
+}
+
+let timer = setInterval(()=>{getToday()},1000)
+
+
+const applicationButtons = document.querySelectorAll(".application")
+for(item of applicationButtons){
+    item.addEventListener('click', ()=>{
+        alert("Оставить заявку")
+    })
+}
