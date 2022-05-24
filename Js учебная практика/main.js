a708e47f09d029ea05a94c842891d993c592da34
Galkin Alexey Andreevich <97546730+GalkinAlexeyAndreevich@users.noreply.github.com>
2022-05-24 20:06:31 +0300

    Add files via upload

--- Js учебная практика/main.js
+++ Js учебная практика/main.js
@@ -1,27 +1,42 @@
-
-
-// getHours(), getMinutes(), getSeconds()
-
 function getToday(){
     const timerItems = document.querySelectorAll(".timer__count")
     console.log(timerItems)
     const today = new Date()
     let arrToday = [today.getDate(),today.getHours(),today.getMinutes(),today.getSeconds()]
-    // let days = today.getDate() 
-    // let hours = today.getHours()
-    // let minutes = today.getMinutes()
-    // let seconds = today.getSeconds() 
     for(let i=0; i<arrToday.length; i++){
         timerItems[i].textContent =  arrToday[i]
     }
 }
 
 let timer = setInterval(()=>{getToday()},1000)
+// const applicationButtons = document.querySelectorAll(".application")
+// for(item of applicationButtons){
+//     item.addEventListener('click', ()=>{
+//         alert("Оставить заявку")
+//     })
+// }
 
 
-const applicationButtons = document.querySelectorAll(".application")
-for(item of applicationButtons){
+
+const modalWindow = document.querySelector('.modal')
+const buttonModals = document.querySelectorAll('.application')
+const modalClose = document.querySelector('.modal__close')
+const body = document.querySelector('.body')
+
+buttonModals.forEach((item) =>{
     item.addEventListener('click', ()=>{
-        alert("Оставить заявку")
+        modalWindow.style.display = ''
+        body.classList.add('noscroll')
     })
-}
+})
+
+modalWindow.addEventListener('click', (e)=>{
+    const isModal = e.target.closest('.modal__inner')
+    if(!isModal){
+        modalWindow.style.display = 'none'
+    }
+})
+
+modalClose.addEventListener('click', ()=>{
+    modalWindow.style.display = 'none'
+})
